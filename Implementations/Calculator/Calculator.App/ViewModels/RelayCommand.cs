c71c91966ba5418f712adb6c61a0dd7f73a11c12
Murali47k <murali47karthick@gmail.com>
2026-09-08 13:05:32 +0530

    added RelayCommand.cs

---
+++ Implementations/Calculator/Calculator.App/ViewModels/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Calculator.App.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
