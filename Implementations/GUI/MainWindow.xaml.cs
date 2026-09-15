5274650af4e39c26ab737cb2f056a5dbc7b24374
Murali47k <murali47karthick@gmail.com>
2026-09-15 15:50:33 +0530

    made simple file reader GUI

--- Implementations/GUI/MainWindow.xaml.cs
+++ Implementations/GUI/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"data.txt");
+
+            if (File.Exists(filePath))
+            {
+                string text = File.ReadAllText(filePath);
+
+                OutputTextBox.Text = text;
+            }
+            else
+            {
+                MessageBox.Show("data.txt file was not found!","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
         }
     }
 }
