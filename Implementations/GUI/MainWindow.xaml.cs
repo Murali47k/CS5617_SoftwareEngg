eabdcc6e50f2804414b10f9d7a0f361d33ed9201
Murali47k <murali47karthick@gmail.com>
2026-09-19 13:34:52 +0530

    added thread to constant read file in file reader GUI

--- Implementations/GUI/MainWindow.xaml.cs
+++ Implementations/GUI/MainWindow.xaml.cs
@@ -1,20 +1,59 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Windows;
 
 namespace GUI
 {
     public partial class MainWindow : Window
     {
+        string filePath;
+        Thread fileThread;
+        DateTime lastModified;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            filePath = Path.Combine(
+                Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)!.Parent!.Parent!.Parent!.FullName,
+                "data.txt"
+            );
+
+            // Store the current modification time
+            if (File.Exists(filePath))
+            {
+                lastModified = File.GetLastWriteTime(filePath);
+            }
+
+            // Start background thread
+            fileThread = new Thread(CheckFile);
+            fileThread.IsBackground = true;
+            fileThread.Start();
         }
 
+        // Background thread
+        private void CheckFile()
+        {
+            while (true)
+            {
+                if (File.Exists(filePath))
+                {
+                    DateTime currentModified = File.GetLastWriteTime(filePath);
+
+                    if (currentModified != lastModified)
+                    {
+                        lastModified = currentModified;
+                    }
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        // Runs on the MAIN/UI thread
         private void ReadFile_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"data.txt");
-
             if (File.Exists(filePath))
             {
                 string text = File.ReadAllText(filePath);
@@ -23,8 +62,14 @@ namespace GUI
             }
             else
             {
-                MessageBox.Show("data.txt file was not found!","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show(
+                    "data.txt file was not found!",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
             }
         }
     }
 }
+
